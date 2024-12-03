f194e7109752400ac24409fbeae60d4c34df6eaf
Alden-laptop <business.aldenlu@gmail.com>
2024-12-03 12:59:23 -0500

    edited makefile to complile and clean + remove code form GFG

--- testing/client.c
+++ testing/client.c
@@ -7,30 +7,5 @@
   
 int main(int argc, char const* argv[]) 
 { 
-    int sockD = socket(AF_INET, SOCK_STREAM, 0); 
-  
-    struct sockaddr_in servAddr; 
-  
-    servAddr.sin_family = AF_INET; 
-    servAddr.sin_port 
-        = htons(9001); // use some unused port number 
-    servAddr.sin_addr.s_addr = INADDR_ANY; 
-  
-    int connectStatus 
-        = connect(sockD, (struct sockaddr*)&servAddr, 
-                  sizeof(servAddr)); 
-  
-    if (connectStatus == -1) { 
-        printf("Error...\n"); 
-    } 
-  
-    else { 
-        char strData[255]; 
-  
-        recv(sockD, strData, sizeof(strData), 0); 
-  
-        printf("Message: %s\n", strData); 
-    } 
-  
-    return 0; 
+    printf("hallword");
 }
